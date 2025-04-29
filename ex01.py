bf7e55f89fa570efa634b80c9b948677b28742d8
Michel <106674127+MichelZero@users.noreply.github.com>
2025-04-29 18:09:28 -0300

    up

--- ex01.py
+++ ex01.py
@@ -5,14 +5,18 @@
 # de tamanho aleatório entre 1 e 20;
 
 #gera tupla dinamicamente
+# Cria um range de 5 elementos e atribui à variável t1
 t1 = range(5)
-print(t1)
+print(t1)  # Imprime o objeto range (não é uma tupla ainda)
 print()
-t1 = tuple(range(5))
-print(t1)
 
-#importar a função randint
+# Converte o range em uma tupla e atribui novamente à variável t1
+t1 = tuple(range(5))
+print(t1)  # Imprime a tupla gerada a partir do range
+
+# Importa a função randint do módulo random para gerar números aleatórios
 from random import randint
 
-t1 = tuple(range(randint(1,20)))
-print(t1)
+# Gera uma tupla dinamicamente com tamanho aleatório entre 1 e 20
+t1 = tuple(range(randint(1, 20)))
+print(t1)  # Imprime a tupla gerada dinamicamente
